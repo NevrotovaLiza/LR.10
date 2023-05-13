e6559b03307fb0a7ea267df7580eba743c421629
nevro <nevrotoval@mail.ru>
2023-05-14 00:10:59 +0700

    Добавьте файлы проекта.

---
+++ MathTaskClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("317c95d3-380d-46df-9c10-6fff39079f10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MathTaskClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary;
+ 
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            // исходные данные
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            // получение значения с помощью тестируемого метода
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CylinderVolume_3and5_235_62returned()
+        {
+            // исходные данные
+            double a = 3;
+            double b = 5;
+            double expected = 141.37;
+
+            // получение значения с помощью тестируемого метода
+            Geometry g = new Geometry();
+            double actual = g.Cylinder(a, b);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
