67274a5605262f1180a52fba26d2132e01b25fdc
nevro <nevrotoval@mail.ru>
2023-05-14 11:56:05 +0700

    LR.11

---
+++ MathTaskClassLibrary/Equation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Equation
+    {
+        public string FindValue(double a, double b, double c)
+        {
+            double D = Math.Pow(b, 2) - 4 * a * c;
+
+            if (D > 0)
+            {
+                double x1 = Math.Round((-b + Math.Sqrt(D)) / (2 * a), 2);
+                double x2 = Math.Round((-b - Math.Sqrt(D)) / (2 * a), 2);
+
+                return $"корни уравнения: {x1}, {x2}";
+            }
+
+            else if (D == 0)
+            {
+                return $"корни уравнения: {-b / 2 * a}";
+            }
+
+            else
+            {
+                return "корней нет";
+            }
+        }
+    }
+}
---
+++ MathTaskClassLibraryTests/EquationTest.cs
@@ -0,0 +1,31 @@
+﻿using MathTaskClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class EquationTest
+    {
+        [TestMethod]
+        public void Discriminant()
+        {
+            //исходные данные
+            int a = 1;
+            int b = 5;
+            int c = 4;
+            string expected = "корни уравнения: -3, -4";
+
+            //получение значения с помощью тестируемого метода
+            Equation e = new Equation();
+            string actual = e.FindValue(a, b, c);
+
+            //сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
